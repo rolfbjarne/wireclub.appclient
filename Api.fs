c2f4180ccf8162ab7d3669c3d0b88df84cb05655
Braden Evans <braden@numericalorder.com>
2014-02-02 14:30:45 -0800

    Flattened api responses

--- Api.fs
+++ Api.fs
@@ -5,6 +5,7 @@ open System.Net
 open System.Net.Http
 open System.Collections.Generic
 open Newtonsoft.Json
+open Wireclub.Boundary
 
 let cookies = new CookieContainer()
 let handler = new HttpClientHandler()
@@ -27,45 +28,44 @@ let baseUrl = "http://192.168.0.102"
 let baseUrl = "http://dev.wireclub.com"
 #endif
 
-type ApiFailureType =
-| Timeout
-| HttpError of int
-| Deserialization
-| Exception
-
 type ApiResult<'A> =
 | ApiOk of 'A
-| ApiError of ApiFailureType * string
+| BadRequest of ApiError[]
+| Unauthorized
+| Timeout
+| HttpError of int * string
+| Deserialization of Exception
+| Exception of Exception
 
 let req<'A> (url:string) (httpMethod:string) (data:(string*string) list)  = async {
     try        
         let url = sprintf "%s/%s" baseUrl url
         let task =
             match httpMethod.ToUpperInvariant() with
-            | "GET" -> 
-                client.GetAsync url
-
-            | "POST" -> 
-                client.PostAsync(url, new FormUrlEncodedContent(data |> List.toArray |> Array.map (fun (k, v) -> KeyValuePair(k,v))))
+            | "GET" -> client.GetAsync url
+            | "POST" -> client.PostAsync(url, new FormUrlEncodedContent(data |> List.map (fun (k, v) -> KeyValuePair(k,v))))
             | _ -> failwithf "Unsupported method: %s" httpMethod
 
         let! resp = Async.AwaitTask task
 
         printfn "HTTP:%s %s %i" httpMethod url (int resp.StatusCode)
         
-        let! content = Async.AwaitTask (resp.Content.ReadAsStringAsync())
+        let! content = Async.AwaitTask (resp.Content.ReadAsStringAsync())        
 
         match int resp.StatusCode with
+        | 200 when typeof<'A> = typeof<string> -> return ApiOk (content :> obj :?> 'A)
         | 200 ->
-            if typeof<'A> = typeof<string> then
-                return ApiOk (content :> obj :?> 'A)
-            else
-                try
-                    return ApiOk (JsonConvert.DeserializeObject<'A>(content))
-                with
-                | ex -> return ApiError (Deserialization, ex.ToString())
-
-        | status -> return ApiError (HttpError status, content)
+            try
+                return ApiOk (JsonConvert.DeserializeObject<'A>(content))
+            with
+            | ex -> return Deserialization ex
+        | 400 -> 
+            try
+                return BadRequest (JsonConvert.DeserializeObject<ApiError[]>(content))
+            with
+            | ex -> return Deserialization ex
+        | 401 -> return Unauthorized
+        | status -> return HttpError (status, content)
     with
-    | ex -> return ApiError (Exception, ex.ToString())
+    | ex -> return Exception ex
 }
