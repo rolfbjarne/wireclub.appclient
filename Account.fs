a6c04afcf38732fba9873f0c3d200c4ef0494bf8
Braden Evans <braden@numericalorder.com>
2014-02-08 14:04:21 -0800

    Token login

--- Account.fs
+++ Account.fs
@@ -4,6 +4,15 @@ open Wireclub.Boundary
 open Wireclub.Boundary.Models
 open Newtonsoft.Json
 
+let handleLogin result =
+    match result with
+    | Api.ApiOk result ->
+        Api.client.DefaultRequestHeaders.TryAddWithoutValidation("x-csrf-token", result.Csrf) |> ignore
+        Api.userId <- result.Identity.Id
+        Api.userHash <- result.Csrf
+        Api.ApiOk result
+    | resp -> resp
+
 let login username password = async {
     let! resp = 
         Api.req<LoginResult> "account/doLogin" "post" (
@@ -12,13 +21,17 @@ let login username password = async {
                 "password", password
             ])
     
-    match resp with
-    | Api.ApiOk result ->
-        Api.client.DefaultRequestHeaders.TryAddWithoutValidation("x-csrf-token", result.Csrf) |> ignore
-        Api.userId <- result.Identity.Id
-        Api.userHash <- result.Csrf
-        return Api.ApiOk result
-    | resp -> return resp
+    return handleLogin resp
+}
+
+let loginToken token = async {
+    let! resp = 
+        Api.req<LoginResult> "account/tokenLogin" "post" (
+            [
+                "token", token
+            ])
+    
+    return handleLogin resp
 }
 
 let home () =
---
+++ Web.fs
@@ -0,0 +1,14 @@
+﻿module Web
+
+open System
+
+let embedUrl url =
+    let appendQuery q (url:string) =
+        if url.Contains "?" then
+            url + "&" + q
+        else
+            url + "?" + q
+
+    url
+    |> appendQuery ("app-session=" + Api.userHash)
+    |> appendQuery "mobileApp=true" // Apps should add the appropriate agent header to all requests, but this is nice for testing
