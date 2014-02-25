3d229338e401bb8dfd4ae9e0f0f4c5fc5133546b
Christopher Hrica <chris.hrica@gmail.com>
2014-02-25 15:23:02 -0800

    Fix Test. Add Places.

---
+++ Places.fs
@@ -0,0 +1,9 @@
+﻿module Places
+
+open Wireclub.Boundary.Models
+
+let countries () =
+    Api.req<LocationCountry[]> "api/places/countries" "post" []
+
+let regions (country:string) =
+    Api.req<LocationRegion[]> "api/places/regions" "post" [ "country", country ]
