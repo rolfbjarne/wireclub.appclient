a6be308e87b8ecfd648aaf873a36f837425ea0e6
Christopher Hrica <chris.hrica@gmail.com>
2014-05-15 17:17:33 -0700

    some progress with native conenctions

--- Wireclub.AppClient/Api.fs
+++ Wireclub.AppClient/Api.fs
@@ -3,14 +3,17 @@
 module Api
 
 open System
+open System.IO
 open System.Net
 open System.Net.Http
 open System.Net.Http.Headers
+open System.Text
 open System.Threading.Tasks
 open System.Collections.Generic
 open System.Diagnostics
 open Newtonsoft.Json
 open Wireclub.Boundary
+open MonoTouch.Foundation
 
 let version = "1.0"
 let mutable agent = "wireclub-app-client/" + version
@@ -30,7 +33,6 @@ handler.AllowAutoRedirect <- false
 let client = new HttpClient(handler)
 client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", agent) |> ignore
 client.DefaultRequestHeaders.Accept.Add(Headers.MediaTypeWithQualityHeaderValue("application/json")) |> ignore
-client.DefaultRequestHeaders.Accept.Add(Headers.MediaTypeWithQualityHeaderValue("image/png")) |> ignore
 
 let debugSlowNetwork = false
 
@@ -51,11 +53,10 @@ staticBaseUrl <- "http://192.168.0.102"
 channelServer <- "wss://192.168.0.102:8888/events"
 #endif
 #if __IOS__
-System.Net.ServicePointManager.ServerCertificateValidationCallback <- new System.Net.Security.RemoteCertificateValidationCallback(fun sender cert chain errors -> true)
-baseUrl <- "https://192.168.0.106"
+baseUrl <- "http://192.168.0.106"
 webUrl <- "http://192.168.0.106"
 staticBaseUrl <- "http://192.168.0.106"
-channelServer <- "wss://192.168.0.106:8888/events"
+channelServer <- "ws://192.168.0.106:8888/events"
 #endif
 #endif
 
@@ -76,13 +77,16 @@ let fullUrl (url:string) =
     | true -> url
     | false -> Uri(Uri(baseUrl), url).ToString()
 
-let respParse<'A> (resp:HttpResponseMessage) = async {
+let respParse<'A> status (data:NSData) = async {
     let stringContent () = 
-            Async.AwaitTask (resp.Content.ReadAsStringAsync())
+        use stream = data.AsStream()
+        use reader = new StreamReader(stream)
+        Async.AwaitTask (reader.ReadToEndAsync())
 
-    match int resp.StatusCode with
+    match int status with
     | 200 when typeof<'A> = typeof<byte[]> -> 
-        let! content = Async.AwaitTask (resp.Content.ReadAsByteArrayAsync ())
+        use stream = data.AsStream()
+        let! content = stream.AsyncRead(int data.Length)
         return ApiOk (content :> obj :?> 'A)
     | 200 when typeof<'A> = typeof<string> -> 
         let! content = stringContent ()
@@ -118,18 +122,46 @@ let req<'A> (url:string) (httpMethod:string) (data:(string*string) list)  = asyn
         if debugSlowNetwork then
             do! Async.Sleep (5 * 1000)
 
-        let task =
-            match httpMethod.ToUpperInvariant() with
-            | "GET" -> client.GetAsync url
-            | "POST" -> client.PostAsync(url, new FormUrlEncodedContent(data |> List.map (fun (k, v) -> KeyValuePair(k,v))))
-            | _ -> failwithf "Unsupported method: %s" httpMethod
+        let headers = new NSMutableDictionary()
+        let request = new NSMutableUrlRequest(NSUrl.FromString url)
+        request.HttpMethod <- httpMethod.ToUpperInvariant()
+        request.ShouldHandleCookies <- true
+        headers.SetValueForKey(NSObject.FromObject (NSString.op_Explicit agent), NSString.op_Explicit "User-Agent")
 
-        let! resp = Async.AwaitTask task
-        do! (awaitTask (resp.Content.LoadIntoBufferAsync()))
+        if httpMethod.ToUpperInvariant() = "POST" then
+            request.Body <- NSData.FromString(String.concat "&" [ for k, v in data do yield sprintf "%s=%s" k v ], NSStringEncoding.UTF8)
+            headers.SetValueForKey(NSObject.FromObject (NSString.op_Explicit (request.Body.Length.ToString())), NSString.op_Explicit "Content-Length")
+            headers.SetValueForKey(NSObject.FromObject (NSString.op_Explicit "application/x-www-form-urlencoded charset=utf-8"), NSString.op_Explicit "Content-Type")
 
-        printfn "[API] %s %s %i %ims" httpMethod url (int resp.StatusCode) (stopwatch.ElapsedMilliseconds)
+        request.Headers <- headers
 
-        let! resp = respParse<'A> resp
+        let! status, data = Async.FromContinuations(fun (cont, econt, _) ->
+            let data = ref (new NSMutableData ())
+            let status = ref 500
+            let connection =
+                new NSUrlConnection(
+                    request,
+                    {
+                        new NSUrlConnectionDelegate() with
+                        override this.ReceivedResponse(connection, response) = 
+                            let response = response :?> NSHttpUrlResponse
+                            status := response.StatusCode
+                            data := new NSMutableData ()
+                        override this.ReceivedData(connection, d) =
+                            (!data).AppendData(d)
+                        override this.FailedWithError(connection, error) =
+                            econt (new Exception(sprintf "%s\n%s" error.Description error.DebugDescription))
+                        override this.FinishedLoading(connection) =
+                            cont (!status, !data)
+                    })
+
+            connection.Start()
+        )
+
+
+        printfn "[API] %s %s %i %ims" httpMethod url status (stopwatch.ElapsedMilliseconds)
+
+        let! resp = (respParse<'A> status data)
         return resp
     with
     | ex -> return Exception ex
@@ -139,31 +171,34 @@ let post<'A> url = req<'A> url "POST" []
 let get<'A> url = req<'A> url "GET" []
 
 let upload<'A> url name filename (data:byte []) =  async {
-    try
-        let stopwatch = Stopwatch()
-        stopwatch.Start()
-
-        let url = fullUrl url
-
-        let content = new MultipartFormDataContent()
-        let fileContent = new ByteArrayContent(data)
-        fileContent.Headers.ContentType <- MediaTypeHeaderValue.Parse("application/octet-stream")
-        fileContent.Headers.ContentDisposition <- new ContentDispositionHeaderValue("form-data");
-        fileContent.Headers.ContentDisposition.Parameters.Add(new NameValueHeaderValue("name", sprintf "\"%s\"" name));
-        fileContent.Headers.ContentDisposition.Parameters.Add(new NameValueHeaderValue("filename", sprintf "\"%s\"" filename));
-
-        content.Add(fileContent)
-        let task = client.PostAsync(url, content)
-
-        let! resp = Async.AwaitTask task
-        do! (awaitTask (resp.Content.LoadIntoBufferAsync()))
-
-        printfn "[API] %s %s %i %ims" "POST" url (int resp.StatusCode) (stopwatch.ElapsedMilliseconds)
-
-        let! resp = respParse<'A> resp
-        return resp
-    with
-    | ex -> return Exception ex
+    let! resp = respParse<'A> 500 (new NSMutableData())
+    return resp
+//
+//    try
+//        let stopwatch = Stopwatch()
+//        stopwatch.Start()
+//
+//        let url = fullUrl url
+//
+//        let content = new MultipartFormDataContent()
+//        let fileContent = new ByteArrayContent(data)
+//        fileContent.Headers.ContentType <- MediaTypeHeaderValue.Parse("application/octet-stream")
+//        fileContent.Headers.ContentDisposition <- new ContentDispositionHeaderValue("form-data");
+//        fileContent.Headers.ContentDisposition.Parameters.Add(new NameValueHeaderValue("name", sprintf "\"%s\"" name));
+//        fileContent.Headers.ContentDisposition.Parameters.Add(new NameValueHeaderValue("filename", sprintf "\"%s\"" filename));
+//
+//        content.Add(fileContent)
+//        let task = client.PostAsync(url, content)
+//
+//        let! resp = Async.AwaitTask task
+//        do! (awaitTask (resp.Content.LoadIntoBufferAsync()))
+//
+//        printfn "[API] %s %s %i %ims" "POST" url (int resp.StatusCode) (stopwatch.ElapsedMilliseconds)
+//
+//        let! resp = respParse<'A> resp
+//        return resp
+//    with
+//    | ex -> return Exception ex
 }
 
 
